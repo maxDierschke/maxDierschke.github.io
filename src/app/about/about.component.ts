db52abe650639bf151110ed5bfd039726a37c8d0
m.dierschke <maximiliant.dierschke@gmail.com>
2025-02-01 12:39:05 +0100

    added initial structure

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ProfileComponent } from './profile/profile.component';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [ProfileComponent],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+
+}
